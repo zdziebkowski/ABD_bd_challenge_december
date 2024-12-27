10eccb4d9466576aa7da5128bfabcab430550f6e
zdziebkowski <wojciech.zdziebkowski@gmail.com>
2024-12-27 07:45:47 +0100

    fix:
    -fetch_weather venv updated
    -gitignore updated

--- weather_fetcher/fetch_weather.bat
+++ weather_fetcher/fetch_weather.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "G:\Data\projekty\Big Data Challenge"
-call .\venv\Scripts\activate
+call .\venv_spark\Scripts\activate
 python -m weather_fetcher.main fetch-current-all --output-dir "weather_fetcher/data/current"
 pause
